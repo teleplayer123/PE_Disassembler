f8df253cc9a50188c8d83e57bf10916cad49d3ca
Cole Sashkin <csashceh1024@gmail.com>
2022-11-14 06:59:22 -0800

    commit on  master

--- pe_disassemble.py
+++ pe_disassemble.py
@@ -1,45 +1,48 @@
 from models.pe_file import PEFile
 import sys
 
+def print_headers():
+    if len(sys.argv) < 2:
+        print(f"Usage: py {sys.argv[0]} <pefile_path>")
+        sys.exit()
+        
+    fn = sys.argv[1]
 
-if len(sys.argv) < 2:
-    print(f"Usage: py {sys.argv[0]} <pefile_path>")
-    sys.exit()
-    
-fn = sys.argv[1]
+    p = PEFile(fn)
+    print("COFF Header")
+    print("-"*16)
+    print(p.coff_hdr)
 
-p = PEFile(fn)
-print("COFF Header")
-print("-"*16)
-print(p.coff_hdr)
+    print("\n\n")
+    print("Standard Header")
+    print("-"*16)
+    print(p.standard_fields)
 
-print("\n\n")
-print("Standard Header")
-print("-"*16)
-print(p.standard_fields)
+    print("\n\n")
+    print("Windows Header")
+    print("-"*16)
+    print(p.win_fields)
 
-print("\n\n")
-print("Windows Header")
-print("-"*16)
-print(p.win_fields)
+    print("\n\n")
+    print("Data Directories")
+    print("-"*16)
+    print(p.data_directories)
 
-print("\n\n")
-print("Data Directories")
-print("-"*16)
-print(p.data_directories)
+    print("\n\n")
+    print("Section Headers")
+    print("-"*16)
+    print(f"Number of Sections: {p.num_of_sections}")
+    print("Section Header 1:")
+    print(p.section_table)
 
-print("\n\n")
-print("Section Headers")
-print("-"*16)
-print(f"Number of Sections: {p.num_of_sections}")
-print("Section Header 1:")
-print(p.section_table)
+    num_sections = p.num_of_sections
+    sect_offset = p.sect_offset
 
-num_sections = p.num_of_sections
-sect_offset = p.sect_offset
+    for i in range(1, num_sections):
+        offset = (i * 40) + sect_offset
+        print(f"\nSection Header {i+1}:")
+        sect = p.get_section_table_entry(offset)
+        print(sect)
 
-for i in range(1, num_sections):
-    offset = (i * 40) + sect_offset
-    print(f"\nSection Header {i+1}:")
-    sect = p.get_section_table_entry(offset)
-    print(sect)
+if __name__ == "__main__":
+    print_headers()
